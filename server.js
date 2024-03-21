f3bc2b3afa3e294543bad7707999cf881e3d23bd
Devinix00 <dpldpl123456@naver.com>
2024-03-21 23:45:52 +0900

    HTTP 라우팅 연습

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
-const axios = require("axios");
+const http = require("http");
 
-axios
-  .get("https://naver.com")
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      res.write("Home");
+      res.end();
+      break;
+
+    case "/about":
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.write("<html>");
+      res.write("<body>");
+      res.write("<h1>About Page</h1>");
+      res.write("</body>");
+      res.write("</html>");
+      res.end();
+      break;
+
+    default:
+      res.writeHead(404);
+      res.end();
+      break;
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
