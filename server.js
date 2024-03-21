a2c036a2d89e2c7f8dad08f2678c6ffeec724595
Devinix00 <dpldpl123456@naver.com>
2024-03-22 01:05:04 +0900

    req.params

--- server.js
+++ server.js
@@ -2,11 +2,35 @@ const express = require("express");
 
 const PORT = 8080;
 
+const Users = [
+  {
+    id: 0,
+    name: "Jack",
+  },
+  {
+    id: 1,
+    name: "Jennifer",
+  },
+];
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world");
+app.get("/users", (req, res) => {
+  res.send(Users);
 });
+
+app.get("/users/:userId", (req, res) => {
+  const userId = Number(req.params.userId);
+  const user = Users[userId];
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.get("/users");
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
