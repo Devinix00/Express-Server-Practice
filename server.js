0ea85d21878a4aade24c9674e183fb2f89078a4a
Devinix00 <dpldpl123456@naver.com>
2024-03-22 00:39:52 +0900

    Express 기본 코드 구조 생성

--- server.js
+++ server.js
@@ -1,46 +1,13 @@
-const http = require("http");
+const express = require("express");
 
 const PORT = 8080;
 
-const targetObject = { a: "a", b: "b" };
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.method === "POST" && req.url === "/") {
-    req.on("data", (data) => {
-      console.log(data);
-      const stringfiedData = data.toString();
-      console.log(stringfiedData);
-      Object.assign(targetObject, JSON.parse(stringfiedData));
-    });
-  } else {
-    if (req.url === "/") {
-      res.writeHead(200, {
-        "Content-Type": "application/json",
-      });
-      res.write(JSON.stringify(targetObject));
-      res.end();
-    } else if (req.url === "/about") {
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-      });
-      res.write("<html>");
-      res.write("<body>");
-      res.write("<h1>About Page</h1>");
-      res.write("</body>");
-      res.write("</html>");
-      res.end();
-    } else {
-      res.writeHead(404);
-      res.end();
-    }
-  }
+app.get("/", (req, res) => {
+  res.send("Hello, world");
 });
 
-fetch("http://localhost:8080", {
-  method: "POST",
-  body: JSON.stringify({ c: "c" }),
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
 });
