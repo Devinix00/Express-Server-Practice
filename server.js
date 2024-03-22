c72e088c81c5059d5a810a1e0aa6e3ff1c97b1aa
Devinix00 <dpldpl123456@naver.com>
2024-03-22 17:46:13 +0900

    Template Engine 사용

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = 8080;
 
 const app = express();
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
@@ -16,6 +19,12 @@ app.use((req, _res, next) => {
   next();
   const diffTime = Date.now() - start;
   console.log(`end: ${req.method} ${req.baseUrl} ${diffTime}ms`);
+});
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    imageTitle: "This is Javascript 2",
+  });
 });
 
 app.use("/users", usersRouter);
