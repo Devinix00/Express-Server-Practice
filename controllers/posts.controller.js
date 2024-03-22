3c1348b5daf9d630edb19ac21267e3b9f0a3f05d
Devinix00 <dpldpl123456@naver.com>
2024-03-22 17:04:23 +0900

    사진 전송 연습

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,5 +1,9 @@
+const path = require("path");
+
 function getPost(req, res) {
-  res.send("<div><h1>Post Title</h1><p>This is a post</p></div>");
+  res.sendFile(
+    path.join(__dirname, "..", "public", "images", "js.png")
+  );
 }
 
 module.exports = {
